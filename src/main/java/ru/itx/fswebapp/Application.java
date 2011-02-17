b3cd25861ced78ca47ebf54bf6d3ff48a3ff7763
Eugene Prokopiev <enp@itx.ru>
2011-02-17 09:54:21 +0300

    add freeswitch client

--- src/main/java/ru/itx/fswebapp/Application.java
+++ src/main/java/ru/itx/fswebapp/Application.java
@@ -19,34 +19,56 @@ package ru.itx.fswebapp;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Node;
+import org.freeswitch.esl.client.inbound.Client;
+import org.freeswitch.esl.client.transport.message.EslMessage;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Date;
+import java.util.List;
 
 public class Application {
 
-	private static class Handler implements HttpHandler {
-		public void handle(HttpExchange request) throws IOException {
-			String response =
-				"<html><head>"+
-				"<meta content=\"text/html; charset=UTF-8\" http-equiv=\"content-type\">"+
-				"<meta http-equiv=\"refresh\" content=\"5; url=/\">"+
-				"<title>Test page</title>"+
-				"</head><body>"+
-				"<b>current date/time - "+new Date()+"</b>"+
-				"</body></html>";
-			request.sendResponseHeaders(200, response.length());
-			OutputStream os = request.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		}
-	}
+	private Client fs;
 
-    public static void main(String[] args) throws Exception {
+	public Application() throws Exception {
+		fs = new Client();
+		fs.connect("pbx", 8021, "ClueCon", 2);
 		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 		server.createContext("/", new Handler());
 		server.start();
+	}
+
+	private class Handler implements HttpHandler {
+		public void handle(HttpExchange request) throws IOException {
+			EslMessage message = fs.sendSyncApiCommand("sofia", "xmlstatus profile stc");
+			String messageBody = "", htmlUsers = "";
+			for (String messageBodyLine : message.getBodyLines())
+				messageBody += messageBodyLine;
+			try {
+				Document document = DocumentHelper.parseText(messageBody);
+				for(Node node : (List<Node>)document.selectNodes("/profile/registrations/registration/user"))
+					htmlUsers += "<li>"+node.getText()+"</li>";
+				String response =
+					"<html><head>"+
+					"<meta content=\"text/html; charset=UTF-8\" http-equiv=\"content-type\">"+
+					"<meta http-equiv=\"refresh\" content=\"5; url=/\">"+
+					"<title>Test page</title>"+
+					"</head><body><ul>"+htmlUsers+"</ul></body></html>";
+				request.sendResponseHeaders(200, response.length());
+				OutputStream os = request.getResponseBody();
+				os.write(response.getBytes());
+				os.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Application();
 	}
 }
